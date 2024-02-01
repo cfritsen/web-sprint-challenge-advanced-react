f2aa02494eed587ae1e33e247fc6686fc035a651
cfritsen <chrisfritsen@gmail.com>
2024-01-31 22:43:25 -0800

    Implemented state, Coordinate grabbing functions

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // Suggested initial states
 const initialMessage = ''
@@ -9,35 +9,59 @@ const initialIndex = 4 // the index the "B" is at
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [index, setIndex] = useState(initialIndex);
 
-  function getXY() {
+  function getXY(index) {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
-
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+    let x = Math.ceil((index + 1) / 3);
+    let y = (index + 1) - ((x - 1) * 3);
+    return `Coordinates (${x}, ${y})`
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setSteps(initialSteps);
+    setIndex(initialIndex);
+    setMessage(initialMessage);
+    setEmail(initialEmail);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    let newIndex;
+    if (direction === 'left'){
+      newIndex = index - 1;
+    }
+    if (direction === 'right'){
+      newIndex = index + 1;
+    }
+    if (direction === 'up'){
+      newIndex = index - 3;
+    }
+    if (direction === 'down'){
+      newIndex = index + 3;
+    }
+
+    return newIndex >= 0 && newIndex <= 8 ? newIndex : index;
+    
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    setIndex(getNextIndex(evt.target.id));
+    setSteps(steps + 1);
   }
 
   function onChange(evt) {
     // You will need this to update the value of the input.
+    
   }
 
   function onSubmit(evt) {
@@ -70,7 +94,7 @@ export default function AppFunctional(props) {
         <button id="reset">reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
+        <input id="email" type="email" placeholder="type email" value="email" onChange={onChange}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
