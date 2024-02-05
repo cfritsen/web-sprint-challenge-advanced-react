641cd453f570dabd39e0668f9172908cc8cf8be3
cfritsen <chrisfritsen@gmail.com>
2024-02-04 23:37:39 -0800

    Added tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,47 @@
+import AppFunctional from './AppFunctional.js'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
+const selectors = document => {
+  left = document.querySelector('#left')
+  right = document.querySelector('#right')
+  up = document.querySelector('#up')
+  down = document.querySelector('#down')
+  email = document.querySelector('#email')
+}
+  
+
+  describe(`Self-Made Tests`, () => {
+    beforeEach(() => {
+      render(<AppFunctional />)
+      selectors(document)
+    })
+    afterEach(() => {
+      document.body.innerHTML = ''
+    })
+
+    test('Left Button says LEFT"', () => {
+      expect(left.textContent).toMatch('LEFT')
+    })
+    test('Right Button says RIGHT"', () => {
+      expect(right.textContent).toMatch('RIGHT')
+    })
+    test('Up Button says UP"', () => {
+      expect(up.textContent).toMatch('UP')
+    })
+    test('Down Button says DOWN"', () => {
+      expect(down.textContent).toMatch('DOWN')
+    })
+    test('Email value changes correctly', () => {
+      fireEvent.change(email, {target: {value: 'test@this.sucks'}})
+      screen.findByText('test@this.sucks')
+    })
+
+  })
+
+
